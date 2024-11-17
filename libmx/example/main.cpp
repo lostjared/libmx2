d658095a8789dda85a432c0666b072442014cc6e
Jared Bruni <jaredbruni@gmail.com>
2024-11-17 15:02:54 -0800

    Add files via upload

--- libmx/example/main.cpp
+++ libmx/example/main.cpp
@@ -1,9 +1,40 @@
 #include"mx.hpp"
 
+class Intro : public obj::Object {
+public:
+    Intro() = default;
+    virtual ~Intro() = default;
+    virtual void load(mx::mxWindow *win) override {
+		font.loadFont("font.ttf", 14);
+	}
+    virtual void draw(mx::mxWindow *win) override {
+        win->text.printText_Solid(font, 25, 25, "Hello, World!");
+    }
+    virtual void event(mx::mxWindow *win, SDL_Event &e) override  {}
+private:
+	mx::Font font;
+};
+
+
+class MainWindow : public mx::mxWindow {
+public:
+    MainWindow() : mx::mxWindow("Hello World", 640, 480, false) {
+        setObject(new Intro());
+		object->load(this);
+    }
+    virtual ~MainWindow() = default;
+    virtual void event(SDL_Event &e) override {}
+    virtual void draw(SDL_Renderer *renderer) override {
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        // draw
+        object->draw(this);
+        SDL_RenderPresent(renderer);
+    }
+};
+
 int main(int argc, char **argv) {
-
-
-
-
-	return 0;
+    MainWindow main_window;
+    main_window.loop();
+    return 0;
 }
