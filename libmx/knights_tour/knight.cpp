25f5c72a586921865fd321618b56806ab68b2b8f
Jared Bruni <jaredbruni@gmail.com>
2024-11-17 21:48:56 -0800

    constexpr

--- libmx/knights_tour/knight.cpp
+++ libmx/knights_tour/knight.cpp
@@ -1,5 +1,8 @@
-#include"mx.hpp"
-#include<algorithm>
+#include "mx.hpp"
+#include <algorithm>
+#include <array>
+#include <cstdlib>
+#include <ctime>
 
 class Tour {
 public:
@@ -13,78 +16,79 @@ private:
     struct Position {
         int row;
         int col;
-        Position(int r = 0, int c = 0) : row(r), col(c) {}
+        constexpr Position(int r = 0, int c = 0) : row(r), col(c) {}
     };
-
+    
     void initializeBoard();
     void clearBoard();
     bool isValidMove(const Position& pos) const;
     int getDegree(const Position& pos) const;
     bool solveKnightsTour(Position pos, int moveCount);
-
-    static const int BOARD_SIZE = 8;
-    int startX = 100;
-    int startY = 30;
+    
+    static constexpr int BOARD_SIZE = 8;
+    static constexpr int TOTAL_MOVES = BOARD_SIZE * BOARD_SIZE + 1;
+    static constexpr int START_X = 100;
+    static constexpr int START_Y = 30;
+    static constexpr int CELL_SIZE = 55;
+    static constexpr int CELL_DRAW_SIZE = 50;
+    static constexpr int KNIGHT_SIZE = 35;
+    
     std::vector<std::vector<int>> board;
     std::vector<Position> moveSequence;
     Position knightPos;
     int moves;
     bool tourOver;
-
-    const int horizontal[8] = {2, 1, -1, -2, -2, -1, 1, 2};
-    const int vertical[8] = {-1, -2, -2, -1, 1, 2, 2, 1};
+    
+    static constexpr std::array<int, 8> horizontal = {2, 1, -1, -2, -2, -1, 1, 2};
+    static constexpr std::array<int, 8> vertical = {-1, -2, -2, -1, 1, 2, 2, 1};
 };
 
 class KnightsTour : public obj::Object {
 public:
     KnightsTour() {}
     ~KnightsTour() override {}
-
+    
     virtual void load(mx::mxWindow *win) override {
-        the_font.loadFont(win->util.getFilePath("data/font.ttf"), 14);
+        the_font.loadFont(win->util.getFilePath("data/font.ttf"), TEXT_SIZE);
         int w = 0, h = 0;
-        knight.loadTexture(win, win->util.getFilePath("data/knight.png"), w, h, true, {255,255,255,255});
+        knight.loadTexture(win, win->util.getFilePath("data/knight.png"), w, h, true, {255, 255, 255, 255});
     }
-
+    
     virtual void draw(mx::mxWindow *win) override {
         tour.drawBoard(win);
         tour.drawKnight(win, knight);
-        win->text.setColor({255,255,255,255});
-        win->text.printText_Blended(the_font, 15, 5, "Knights Tour - Tap Space, Press Return to Reset");
-        win->text.printText_Blended(the_font, 400, 5, "Moves: " + std::to_string(tour.getMoves()));
+        win->text.setColor({255, 255, 255, 255});
+        win->text.printText_Blended(the_font, TEXT_OFFSET_X, TEXT_OFFSET_Y, "Knights Tour - Tap Space, Press Return to Reset");
+        win->text.printText_Blended(the_font, 400, TEXT_OFFSET_Y, "Moves: " + std::to_string(tour.getMoves()));
     }
-
-    virtual void event(mx::mxWindow *win, SDL_Event &e) override  {
-        switch(e.type) {
+    
+    virtual void event(mx::mxWindow *win, SDL_Event &e) override {
+        switch (e.type) {
             case SDL_KEYDOWN:
-            switch(e.key.keysym.sym) {
-                case SDLK_SPACE:
-                tour.nextMove();
+                switch (e.key.keysym.sym) {
+                    case SDLK_SPACE:
+                        tour.nextMove();
+                        break;
+                    case SDLK_RETURN:
+                        tour.resetTour();
+                        break;
+                }
                 break;
-                case SDLK_RETURN:
-                tour.resetTour();
-                break;
-            }
-            break;
         }
     }
 private:
     mx::Font the_font;
     mx::Texture knight;
-    static const int BOARD_SIZE = 8;
-    struct Position {
-        Position() = default;
-        int row = 0;
-        int col = 0;
-        Position(int r, int c) : row(r), col(c) {}
-    };
-
+    
+    static constexpr int TEXT_OFFSET_X = 15;
+    static constexpr int TEXT_OFFSET_Y = 5;
+    static constexpr int TEXT_SIZE = 14;
+    
     Tour tour;
 };
 
-
 Tour::Tour() : moves(1), tourOver(false) {
-    srand(static_cast<int>(time(0)));
+    srand(static_cast<unsigned int>(time(0)));
     initializeBoard();
     resetTour();
 }
@@ -101,8 +105,8 @@ void Tour::clearBoard() {
 
 bool Tour::isValidMove(const Position& pos) const {
     return pos.row >= 0 && pos.row < BOARD_SIZE &&
-           pos.col >= 0 && pos.col < BOARD_SIZE &&
-           board[pos.row][pos.col] == 0;
+    pos.col >= 0 && pos.col < BOARD_SIZE &&
+    board[pos.row][pos.col] == 0;
 }
 
 int Tour::getDegree(const Position& pos) const {
@@ -118,12 +122,12 @@ int Tour::getDegree(const Position& pos) const {
 }
 
 bool Tour::solveKnightsTour(Position pos, int moveCount) {
-    if (moveCount == BOARD_SIZE * BOARD_SIZE + 1) {
+    if (moveCount == TOTAL_MOVES) {
         return true;
     }
-
+    
     std::vector<std::pair<int, Position>> nextMoves;
-
+    
     for (int i = 0; i < 8; ++i) {
         Position newPos(pos.row + vertical[i], pos.col + horizontal[i]);
         if (isValidMove(newPos)) {
@@ -131,16 +135,16 @@ bool Tour::solveKnightsTour(Position pos, int moveCount) {
             nextMoves.emplace_back(degree, newPos);
         }
     }
-
+    
     std::sort(nextMoves.begin(), nextMoves.end(),
               [](const std::pair<int, Position>& a, const std::pair<int, Position>& b) {
-                  return a.first < b.first;
+        return a.first < b.first;
     });
-
+    
     for (const auto& [degree, nextPos] : nextMoves) {
         board[nextPos.row][nextPos.col] = moveCount;
         moveSequence.push_back(nextPos);
-
+        
         if (solveKnightsTour(nextPos, moveCount + 1)) {
             return true;
         } else {
@@ -148,7 +152,7 @@ bool Tour::solveKnightsTour(Position pos, int moveCount) {
             moveSequence.pop_back();
         }
     }
-
+    
     return false;
 }
 
@@ -164,19 +168,18 @@ void Tour::resetTour() {
 }
 
 void Tour::nextMove() {
-
-    if(moves == 65) {
+    if (moves == TOTAL_MOVES) {
         return;
     }
-
+    
     if (!moveSequence.empty()) {
         Position nextPos = moveSequence[moves - 1];
-        board[knightPos.row][knightPos.col] = -1; 
+        board[knightPos.row][knightPos.col] = -1;
         knightPos = nextPos;
         board[knightPos.row][knightPos.col] = moves;
         ++moves;
-
-        if (moves == BOARD_SIZE * BOARD_SIZE + 1) {
+        
+        if (moves == TOTAL_MOVES) {
             tourOver = true;
         }
     }
@@ -187,7 +190,7 @@ void Tour::drawBoard(mx::mxWindow* win) {
     SDL_RenderClear(win->renderer);
     for (int i = 0; i < BOARD_SIZE; ++i) {
         for (int j = 0; j < BOARD_SIZE; ++j) {
-            SDL_Rect rect = {startX + j * 55, startY + i * 55, 50, 50};
+            SDL_Rect rect = {START_X + j * CELL_SIZE, START_Y + i * CELL_SIZE, CELL_DRAW_SIZE, CELL_DRAW_SIZE};
             if (board[i][j] == -1) {
                 SDL_SetRenderDrawColor(win->renderer, 0, 0, 0, 255);
             } else if ((i + j) % 2 == 0) {
@@ -201,41 +204,44 @@ void Tour::drawBoard(mx::mxWindow* win) {
 }
 
 void Tour::drawKnight(mx::mxWindow* win, mx::Texture& tex) {
-    SDL_Rect dst = {startX + knightPos.col * 55 + 5, startY + knightPos.row * 55 + 5, 35, 35};
+    SDL_Rect dst = {START_X + knightPos.col * CELL_SIZE + 5, START_Y + knightPos.row * CELL_SIZE + 5, KNIGHT_SIZE, KNIGHT_SIZE};
     SDL_RenderCopy(win->renderer, tex.handle(), nullptr, &dst);
 }
 
 class MainWindow : public mx::mxWindow {
 public:
-    MainWindow(const std::string &path) : mx::mxWindow("Knights Tour", 640, 480, false) {
+    static constexpr int WINDOW_WIDTH = 640;
+    static constexpr int WINDOW_HEIGHT = 480;
+    
+    MainWindow(const std::string &path)
+    : mx::mxWindow("Knights Tour", WINDOW_WIDTH, WINDOW_HEIGHT, false) {
         setPath(path);
         setObject(new KnightsTour());
         object->load(this);
     }
     
-    ~MainWindow() override {
-        
-    }
-
+    ~MainWindow() override {}
+    
     virtual void event(SDL_Event &e) override {
-        
-    }
 
+    }
+    
     virtual void draw(SDL_Renderer *renderer) override {
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
-        // draw
         object->draw(this);
         SDL_RenderPresent(renderer);
     }
 };
 
 int main(int argc, char **argv) {
-    if(argc == 2) {
+    if (argc == 2) {
         MainWindow main_window(argv[1]);
         main_window.loop();
         return 0;
     } else {
-        mx::system_err << "Error requires argument of path to resources.\n";
+        mx::system_err << "Error: Requires argument of path to resources.\n";
+        return 1;
     }
 }
+
