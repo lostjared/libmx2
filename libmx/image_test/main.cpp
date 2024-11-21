b0ec5fc25676ed6910d7599d34fec88d350934b9
Jared Bruni <jaredbruni@gmail.com>
2024-11-21 04:50:21 -0800

    image test path
    image tst path

--- libmx/image_test/main.cpp
+++ libmx/image_test/main.cpp
@@ -1,4 +1,5 @@
 #include"mx.hpp"
+#include"argz.hpp"
 
 class Intro : public obj::Object {
 public:
@@ -20,7 +21,8 @@ private:
 
 class MainWindow : public mx::mxWindow {
 public:
-    MainWindow(std::string path) : mx::mxWindow("Hello World", 640, 480, false) {
+    MainWindow(std::string path, int tw, int th) : mx::mxWindow("Hello World", tw, th, false) {
+        tex.createTexture(this, 640, 480);
       	setPath(path);
         setObject(new Intro());
 		object->load(this);
@@ -30,24 +32,89 @@ public:
     }
     virtual void event(SDL_Event &e) override {}
     virtual void draw(SDL_Renderer *renderer) override {
+        SDL_SetRenderTarget(renderer, tex.wrapper().unwrap());
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
         // draw
         object->draw(this);
+        SDL_SetRenderTarget(renderer,nullptr);
+        SDL_RenderCopy(renderer, tex.wrapper().unwrap(), nullptr, nullptr);
         SDL_RenderPresent(renderer);
     }
+private:
+    mx::Texture tex;
 };
 
+MainWindow *main_w = nullptr;
+
+void eventProc() {
+    main_w->proc();
+}
+
 int main(int argc, char **argv) {
-    if(argc == 2) {
-        try {
-            MainWindow main_window(argv[1]);
-            main_window.loop();
-        } catch(const mx::Exception &e) {
-            mx::system_err << "mx: " << e.text() << "\n";
-            mx::system_err.flush();
-            exit(EXIT_FAILURE);
+#ifdef __EMSCRIPTEN__
+    MainWindow main_window("/");
+    main_w =&main_window;
+    emscripten_set_main_loop(eventProc, 0, 1);
+#else
+
+    Argz<std::string> parser(argc, argv);    
+    parser.addOptionSingle('h', "Display help message")
+          .addOptionSingleValue('p', "assets path")
+          .addOptionDoubleValue('P', "path", "assets path")
+          .addOptionSingleValue('r',"Resolution WidthxHeight")
+          .addOptionDoubleValue('R',"resolution", "Resolution WidthxHeight");
+    Argument<std::string> arg;
+    std::string path;
+    int value = 0;
+    int tw = 1440, th = 1080;
+    try {
+        while((value = parser.proc(arg)) != -1) {
+            switch(value) {
+                case 'h':
+                case 'v':
+                    parser.help(std::cout);
+                    exit(EXIT_SUCCESS);
+                    break;
+                case 'p':
+                case 'P':
+                    path = arg.arg_value;
+                break;
+                case 'r':
+                case 'R': {
+                    auto pos = arg.arg_value.find("x");
+                    if(pos == std::string::npos)  {
+                        mx::system_err << "Error invalid resolution use WidthxHeight\n";
+                        mx::system_err.flush();
+                        exit(EXIT_FAILURE);
+                    }
+                    std::string left, right;
+                    left = arg.arg_value.substr(0, pos);
+                    right = arg.arg_value.substr(pos+1);
+                    tw = atoi(left.c_str());
+                    th = atoi(right.c_str());
+                }
+                break;
+
+            }
         }
+    } catch (const ArgException<std::string>& e) {
+        mx::system_err << e.text() << "\n";
     }
+
+    if(path.empty()) {
+        mx::system_err << "Test: Requires path variable to assets...\n";
+        mx::system_err.flush();
+        exit(EXIT_FAILURE);
+    }
+    try {
+        MainWindow main_window(path, tw, th);
+        main_window.loop();
+    } catch(const mx::Exception &e) {
+        mx::system_err << "mx: Exception: " << e.text() << "\n";
+        mx::system_err.flush();
+        exit(EXIT_FAILURE);
+    } 
+#endif
     return 0;
 }
