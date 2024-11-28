10bfd306977c58478a830ed442dc1cf6aca40f8c
Jared Bruni <jaredbruni@gmail.com>
2024-11-28 06:07:25 -0800

    gl_skeleton
    skeleton

---
+++ libmx/gl_skeleton/skeleton.cpp
@@ -0,0 +1,275 @@
+#include"mx.hpp"
+#include"argz.hpp"
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten/emscripten.h>
+#include <GLES3/gl3.h>
+#define WITH_GL
+#endif
+
+#include"gl.hpp"
+#include"loadpng.hpp"
+
+#define CHECK_GL_ERROR() \
+{ GLenum err = glGetError(); \
+if (err != GL_NO_ERROR) \
+printf("OpenGL Error: %d at %s:%d\n", err, __FILE__, __LINE__); }
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+GLuint loadTexture(const std::string &path) {
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    SDL_Surface *surface = png::LoadPNG(path.c_str());
+    if (!surface) {
+        throw mx::Exception("Failed to load texture: " + path);
+    }
+    
+    GLenum format = (surface->format->BytesPerPixel == 4) ? GL_RGBA : GL_RGB;
+    glTexImage2D(GL_TEXTURE_2D, 0, format, surface->w, surface->h, 0, format, GL_UNSIGNED_BYTE, surface->pixels);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    
+    SDL_FreeSurface(surface);
+    return textureID;
+}
+
+
+class Game : public gl::GLObject {
+public:
+    
+    gl::ShaderProgram textShader;
+
+    Game() = default;
+    virtual ~Game() override {}
+
+    void load(gl::GLWindow *win) override {
+        font.loadFont(win->util.getFilePath("data/font.ttf"), 36);
+        textShader.loadProgram(win->util.getFilePath("data/text.vert"), win->util.getFilePath("data/text.frag"));
+    }
+
+    void draw(gl::GLWindow *win) override {
+        glDisable(GL_DEPTH_TEST);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        textShader.useProgram();
+        SDL_Color white = {255, 255, 255, 255};
+        int textWidth = 0, textHeight = 0;
+        GLuint textTexture = createTextTexture("Hello, World!", font.wrapper().unwrap(), white, textWidth, textHeight);
+        renderText(textTexture, textWidth, textHeight, win->w, win->h);
+        glDeleteTextures(1, &textTexture);
+
+        Uint32 currentTime = SDL_GetTicks();
+        float deltaTime = (currentTime - lastUpdateTime) / 1000.0f; // Convert to seconds
+        lastUpdateTime = currentTime;
+        update(deltaTime);
+    }
+    
+    void event(gl::GLWindow *win, SDL_Event &e) override {}
+    void update(float deltaTime) {}
+    
+    SDL_Surface* flipSurface(SDL_Surface* surface) {
+        SDL_LockSurface(surface);
+        Uint8* pixels = (Uint8*)surface->pixels;
+        int pitch = surface->pitch;
+        Uint8* tempRow = new Uint8[pitch];
+        for (int y = 0; y < surface->h / 2; ++y) {
+            Uint8* row1 = pixels + y * pitch;
+            Uint8* row2 = pixels + (surface->h - y - 1) * pitch;
+            memcpy(tempRow, row1, pitch);
+            memcpy(row1, row2, pitch);
+            memcpy(row2, tempRow, pitch);
+        }
+        delete[] tempRow;
+        SDL_UnlockSurface(surface);
+        return surface;
+    }
+    
+    GLuint createTextTexture(const std::string &text, TTF_Font *font, SDL_Color color, int &textWidth, int &textHeight) {
+        SDL_Surface *surface = TTF_RenderText_Blended(font, text.c_str(), color);
+        if (!surface) {
+            mx::system_err << "Failed to create text surface: " << TTF_GetError() << std::endl;
+            return 0;
+        }
+        
+        surface = flipSurface(surface);
+        
+        textWidth = surface->w;
+        textHeight = surface->h;
+        
+        if (surface->format->format != SDL_PIXELFORMAT_RGBA32) {
+            SDL_Surface *converted = SDL_ConvertSurfaceFormat(surface, SDL_PIXELFORMAT_RGBA32, 0);
+            if (!converted) {
+                mx::system_err << "Failed to convert surface format: " << SDL_GetError() << std::endl;
+                SDL_FreeSurface(surface);
+                return 0;
+            }
+            SDL_FreeSurface(surface);
+            surface = converted;
+        }
+        
+        GLuint texture;
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, textWidth, textHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, surface->pixels);
+        
+        GLenum error = glGetError();
+        if (error != GL_NO_ERROR) {
+            mx::system_err << "OpenGL Error: " << error << " while creating texture." << std::endl;
+            SDL_FreeSurface(surface);
+            return 0;
+        }
+        SDL_FreeSurface(surface);
+        return texture;
+    }
+        
+    void renderText(GLuint texture, int textWidth, int textHeight, int screenWidth, int screenHeight) {
+        float x = -0.5f * (float)textWidth / screenWidth * 2.0f;
+        float y = 0.9f;
+        float w = (float)textWidth / screenWidth * 2.0f;
+        float h = (float)textHeight / screenHeight * 2.0f;
+        
+        GLfloat vertices[] = {
+            x,     y,      0.0f, 0.0f, 1.0f,
+            x + w, y,      0.0f, 1.0f, 1.0f,
+            x,     y - h,  0.0f, 0.0f, 0.0f,
+            x + w, y - h,  0.0f, 1.0f, 0.0f
+        };
+        
+        GLuint VBO, VAO;
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+        
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+        
+        textShader.useProgram();
+        textShader.setUniform("textTexture", 0);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+        
+        glBindVertexArray(0);
+        glDeleteBuffers(1, &VBO);
+        glDeleteVertexArrays(1, &VAO);
+    }
+private:
+    mx::Font font;
+    Uint32 lastUpdateTime = SDL_GetTicks();
+};
+
+class MainWindow : public gl::GLWindow {
+public:
+    MainWindow(std::string path, int tw, int th) : gl::GLWindow("Skeleton", tw, th) {
+        setPath(path);
+        setObject(new Game());
+        object->load(this);
+    }
+    
+    ~MainWindow() override {}
+    
+    virtual void event(SDL_Event &e) override {
+        object->event(this, e);
+    }
+    
+    virtual void draw() override {
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glEnable(GL_DEPTH_TEST);
+        glViewport(0, 0, w, h);
+        object->draw(this);
+        swap();
+        delay();
+    }
+};
+
+MainWindow *main_w = nullptr;
+
+void eventProc() {
+    main_w->proc();
+}
+
+int main(int argc, char **argv) {
+#ifdef __EMSCRIPTEN__
+    MainWindow main_window("/", 960, 720);
+    main_w =&main_window;
+    emscripten_set_main_loop(eventProc, 0, 1);
+#else
+    Argz<std::string> parser(argc, argv);
+    parser.addOptionSingle('h', "Display help message")
+        .addOptionSingleValue('p', "assets path")
+        .addOptionDoubleValue('P', "path", "assets path")
+        .addOptionSingleValue('r',"Resolution WidthxHeight")
+        .addOptionDoubleValue('R',"resolution", "Resolution WidthxHeight");
+    Argument<std::string> arg;
+    std::string path;
+    int value = 0;
+    int tw = 960, th = 720;
+    try {
+        while((value = parser.proc(arg)) != -1) {
+            switch(value) {
+                case 'h':
+                case 'v':
+                    parser.help(std::cout);
+                    exit(EXIT_SUCCESS);
+                    break;
+                case 'p':
+                case 'P':
+                    path = arg.arg_value;
+                    break;
+                case 'r':
+                case 'R': {
+                    auto pos = arg.arg_value.find("x");
+                    if(pos == std::string::npos)  {
+                        mx::system_err << "Error invalid resolution use WidthxHeight\n";
+                        mx::system_err.flush();
+                        exit(EXIT_FAILURE);
+                    }
+                    std::string left, right;
+                    left = arg.arg_value.substr(0, pos);
+                    right = arg.arg_value.substr(pos+1);
+                    tw = atoi(left.c_str());
+                    th = atoi(right.c_str());
+                }
+                    break;
+            }
+        }
+    } catch (const ArgException<std::string>& e) {
+        mx::system_err << e.text() << "\n";
+    }
+    if(path.empty()) {
+        mx::system_out << "mx: No path provided trying default current directory.\n";
+        path = ".";
+    }
+    try {
+        MainWindow main_window(path, tw, th);
+        main_window.loop();
+    } catch(const mx::Exception &e) {
+        mx::system_err << "mx: Exception: " << e.text() << "\n";
+        mx::system_err.flush();
+        exit(EXIT_FAILURE);
+    }
+#endif
+    return 0;
+}
