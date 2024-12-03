492401f6c07afb9444b82529ab4e5127dbb57de3
Jared Bruni <jaredbruni@gmail.com>
2024-12-02 17:18:52 -0800

    update SDL_Init
    update

--- libmx/libmx/gl.cpp
+++ libmx/libmx/gl.cpp
@@ -24,7 +24,7 @@ namespace gl {
 
     void GLWindow::initGL(const std::string &title, int width, int height) {
 
-        if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK) < 0) {
+        if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS | SDL_INIT_JOYSTICK | SDL_INIT_GAMECONTROLLER) < 0) {
             mx::system_err << "Error initalizing SDL.\n";
             mx::system_err.flush();
             exit(EXIT_FAILURE);
