37d01cc21b448502fc43bffbca338deda8437b15
Jared Bruni <jaredbruni@gmail.com>
2025-01-05 18:11:53 -0800

    temporary directory
    same to temp

--- libmx/libmx/tee_stream.cpp
+++ libmx/libmx/tee_stream.cpp
@@ -1,4 +1,5 @@
 #include"tee_stream.hpp"
+#include<filesystem>
 
 namespace mx {
     static std::ofstream log_file("system.log.txt", std::ios::out);
@@ -7,12 +8,20 @@ namespace mx {
     TeeStream system_err(std::cerr, error_file);
     
      void redirect() {
+        std::filesystem::path tempDir = std::filesystem::temp_directory_path();
         if (!log_file.is_open()) {
 #ifndef _WIN32
             log_file.open("/tmp/system.log.txt", std::ios::out);
             if(!log_file.is_open()) {
                 std::cerr << "Error creating log file.\n";
             }
+#else
+            std::filesystem::path logFilePath = tempDir / "system.log.txt";
+            log_file.open(logFilePath, std::ios::out);
+            if(!log_file.is_open()) {
+                std::cerr << "Error";
+            }
+            system_out << "Redirected to: " << logFilePath << "\n";         
 #endif
         }
         if (!error_file.is_open()) {
@@ -21,6 +30,13 @@ namespace mx {
             if(!error_file.is_open())  {
                 std::cerr << "Error creating error log.\n";
             }
+#else
+            std::filesystem::path logFilePath = tempDir / "error.log.txt";
+            error_file.open(logFilePath, std::ios::out);
+            if(!log_file.is_open()) {
+                std::cerr << "Error";
+            }
+            system_out << "Redirected to: " << logFilePath << "\n";
 #endif
         }
 
