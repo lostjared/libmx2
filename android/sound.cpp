75aa5da787c2ab9fb904f861726cfdddff3b6e2d
Jared Bruni <jaredbruni@gmail.com>
2025-02-11 18:17:53 -0800

    updated sound
    updated sound

--- android/sound.cpp
+++ android/sound.cpp
@@ -13,23 +13,47 @@ namespace mx {
 
         }
         Mixer::~Mixer() {
-            if(init_) {
-                for (auto &m : files) {
-                    Mix_FreeMusic(m);
+           cleanup();
+        }
+
+        void Mixer::cleanup() {
+            if(init_ == true) {
+                stopMusic();
+                for(auto &i : files) {
+                    if(i != nullptr) {
+                        Mix_FreeMusic(i);
+                        i = nullptr;
+                    }
                 }
-                for(auto &m: wav) {
-                    Mix_FreeChunk(m);
+                for(auto &i : wav) {
+                    if(i != nullptr) {
+                        Mix_FreeChunk(i);
+                        i = nullptr;
+                    }
                 }
+                files.clear();
+                wav.clear();
                 Mix_CloseAudio();
+                Mix_Quit();
+                init_ = false;
             }
+
         }
         
+        void Mixer::stopMusic() {
+            if (Mix_QuerySpec(nullptr, nullptr, nullptr) != 0) {
+                if (Mix_PlayingMusic()) {
+                    Mix_HaltMusic();
+                }
+                Mix_HaltChannel(-1);
+            }
+        }
+
         void Mixer::init() {
             if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
-                //throw mx::Exception("Could not initalize: " + std::string(Mix_GetError()));
-                return;
+                throw mx::Exception("Could not initalize: " + std::string(Mix_GetError()));
             }
-            ////std::cout << "mx: Mixer: initalized...\n";
+            //mx::system_out << "mx: Mixer: initalized...\n";
             init_ = true;
         }
 
@@ -37,8 +61,7 @@ namespace mx {
             Mix_Music *m = Mix_LoadMUS(filename.c_str());
             if(!m) {
                 Mix_CloseAudio();
-                //throw mx::Exception("Error loading music file: " + filename + ": " +std::string(Mix_GetError()));
-                return -1;
+                throw mx::Exception("Error loading music file: " + filename + ": " +std::string(Mix_GetError()));
             }
             files.push_back(m);
             return static_cast<int>(files.size()-1);
@@ -49,7 +72,7 @@ namespace mx {
                 if (Mix_PlayMusic(files.at(id), value) == -1) {
                     Mix_FreeMusic(files.at(id));
                     Mix_CloseAudio();
-                    //throw mx::Exception("Play music failed: " + std::string(Mix_GetError()));
+                    throw mx::Exception("Play music failed: " + std::string(Mix_GetError()));
                     return -1;
                 }
             } else {
@@ -61,7 +84,7 @@ namespace mx {
             Mix_Chunk *m = Mix_LoadWAV(filename.c_str());
             if(!m) {
                 Mix_CloseAudio();
-                //throw mx::Exception("Error loading file: " + std::string(Mix_GetError()));
+                throw mx::Exception("Error loading file: " + std::string(Mix_GetError()));
                 return -1;
             }
             wav.push_back(m);
