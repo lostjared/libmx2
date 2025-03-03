7dcf106f4abc7b4b0c4e78fdb9d0896e89881934
Jared Bruni <jaredbruni@gmail.com>
2025-03-03 00:44:08 -0800

    3d cube
    cube

--- libmx/gl_cube/example.cpp
+++ libmx/gl_cube/example.cpp
@@ -8,213 +8,152 @@
 
 #include"gl.hpp"
 #include"loadpng.hpp"
+#include"model.hpp"
 
 #define CHECK_GL_ERROR() \
     { GLenum err = glGetError(); \
       if (err != GL_NO_ERROR) \
         printf("OpenGL Error: %d at %s:%d\n", err, __FILE__, __LINE__); }
+#if defined(__EMSCRIPTEN__) || defined(__ANDOIRD__)
+    const char *g_vSource = R"(#version 300 es
+        precision highp float;
+        layout (location = 0) in vec3 aPos;
+        layout (location = 1) in vec3 aNormal;
+        layout (location = 2) in vec2 aTexCoords;
+        uniform mat4 model;
+        uniform mat4 view;
+        uniform mat4 projection;
 
+        uniform vec3 lightPos;
+        uniform vec3 viewPos;
+        uniform vec3 lightColor;
+        uniform vec3 objectColor;
 
-GLfloat vertices[] = { -1.0f, -1.0f, 1.0f, // front face
-    1.0f, 1.0f, 1.0f,
-    -1.0f, 1.0f, 1.0f,
-    
-    -1.0f, -1.0f, 1.0f,
-    1.0f, -1.0f, 1.0f,
-    1.0f, 1.0f, 1.0f,
-    
-    -1.0f, -1.0f, -1.0f, // left side
-    -1.0f, -1.0f, 1.0f,
-    -1.0f, 1.0f, -1.0f,
-    
-    -1.0f, 1.0f, -1.0f,
-    -1.0f, -1.0f, 1.0f,
-    -1.0f, 1.0f, 1.0f,
-    
-    -1.0f, 1.0f, -1.0f, // top
-    -1.0f, 1.0f, 1.0f,
-    1.0f, 1.0f, 1.0f,
-    
-    1.0f, 1.0f, 1.0f,
-    1.0f, 1.0f, -1.0f,
-    -1.0f, 1.0f, -1.0f,
-    
-    -1.0f, -1.0f, -1.0f, // bottom
-    -1.0f, -1.0f, 1.0f,
-    1.0f, -1.0f, 1.0f,
-    
-    1.0f, -1.0f, 1.0f,
-    1.0f, -1.0f, -1.0f,
-    -1.0f, -1.0f, -1.0f,
-    
-    1.0f, -1.0f, -1.0f, // right
-    1.0f, -1.0f, 1.0f,
-    1.0f, 1.0f, -1.0f,
-    
-    1.0f, 1.0f, -1.0f,
-    1.0f, -1.0f, 1.0f,
-    1.0f, 1.0f, 1.0f,
-    
-    -1.0f, -1.0f, -1.0f, // back face
-    1.0f, 1.0f, -1.0f,
-    -1.0f, 1.0f, -1.0f,
-    
-    -1.0f, -1.0f, -1.0f,
-    1.0f, -1.0f, -1.0f,
-    1.0f, 1.0f, -1.0f,
-    
-};
+        out vec3 vertexColor;
+        out vec2 TexCoords;
 
-GLfloat texCoords[] = {
-    0, 1, // Front face
-    1, 0,
-    0, 0,
-    
-    0, 1,
-    1, 1,
-    1, 0,
-    
-    0, 1, // Left face
-    1, 1,
-    0, 0,
-    
-    0, 0,
-    1, 1,
-    1, 0,
-    
-    0, 1, // Top face
-    0, 0,
-    1, 0,
-    
-    1, 0,
-    1, 1,
-    0, 1,
-    
-    0, 1, // Bottom face
-    0, 0,
-    1, 0,
-    
-    1, 0,
-    1, 1,
-    0, 1,
-    
-    0, 1, // Right face
-    1, 1,
-    0, 0,
-    
-    0, 0,
-    1, 1,
-    1, 0,
-    
-    0, 1, // Back face
-    1, 0,
-    0, 0,
-    
-    0, 1,
-    1, 1,
-    1, 0
-};
+        void main()
+        {
+            vec4 worldPos = model * vec4(aPos, 1.0);
+            gl_Position = projection * view * worldPos;
+            vec3 norm = normalize(mat3(transpose(inverse(model))) * aNormal);
+            vec3 lightDir = normalize(lightPos - vec3(worldPos));
+            float ambientStrength = 0.1;
+            vec3 ambient = ambientStrength * lightColor;
+            float diff = max(dot(norm, lightDir), 0.0);
+            vec3 diffuse = diff * lightColor;
+            float specularStrength = 1.0;    // Increase from 0.5 to 1.0 for more shine
+            float shininess = 64.0;          // Increase this value for a tighter, shinier highlight
+            vec3 viewDir = normalize(viewPos - vec3(worldPos));
+            vec3 reflectDir = reflect(-lightDir, norm);
+            float spec = pow(max(dot(viewDir, reflectDir), 0.0), shininess);
+            vec3 specular = specularStrength * spec * lightColor;
+            vec3 finalColor = (ambient + diffuse + specular) * objectColor;
+            vertexColor = finalColor;
+            TexCoords = aTexCoords;
+        }
+    )";
 
+    const char *g_fSource = R"(#version 300 es
+            precision highp float;
+            in vec3 vertexColor;
+            in vec2 TexCoords;
+            uniform sampler2D texture1;  
+            out vec4 FragColor;
+
+            void main()
+            {
+                FragColor = vec4(vertexColor, 1.0) * texture(texture1, TexCoords);
+                FragColor.a =  0.5;
+            }
+    )";
+    #else
+    const char *g_vSource = R"(#version 330 core
+        layout (location = 0) in vec3 aPos;
+
+        layout (location = 1) in vec3 aNormal;
+        layout (location = 2) in vec2 aTexCoords;
+        uniform mat4 model;
+        uniform mat4 view;
+        uniform mat4 projection;
+
+        uniform vec3 lightPos;
+        uniform vec3 viewPos;
+        uniform vec3 lightColor;
+        uniform vec3 objectColor;
+
+        out vec3 vertexColor;
+        out vec2 TexCoords;
+
+        void main()
+        {
+            vec4 worldPos = model * vec4(aPos, 1.0);
+            gl_Position = projection * view * worldPos;
+            vec3 norm = normalize(mat3(transpose(inverse(model))) * aNormal);
+            vec3 lightDir = normalize(lightPos - vec3(worldPos));
+            float ambientStrength = 0.1;
+            vec3 ambient = ambientStrength * lightColor;
+            float diff = max(dot(norm, lightDir), 0.0);
+            vec3 diffuse = diff * lightColor;
+            float specularStrength = 1.0;    // Increase from 0.5 to 1.0 for more shine
+            float shininess = 64.0;          // Increase this value for a tighter, shinier highlight
+            vec3 viewDir = normalize(viewPos - vec3(worldPos));
+            vec3 reflectDir = reflect(-lightDir, norm);
+            float spec = pow(max(dot(viewDir, reflectDir), 0.0), shininess);
+            vec3 specular = specularStrength * spec * lightColor;
+            vec3 finalColor = (ambient + diffuse + specular) * objectColor;
+            vertexColor = finalColor;
+            TexCoords = aTexCoords;
+        }
+
+    )";
+    const char *g_fSource = R"(#version 330 core
+            in vec3 vertexColor;
+            in vec2 TexCoords;
+            uniform sampler2D texture1;  
+            out vec4 FragColor;
+
+            void main()
+            {
+                FragColor = vec4(vertexColor, 1.0) * texture(texture1, TexCoords);
+                FragColor.a = 0.5;
+            }
+    )";
+#endif
 
 class Cube : public gl::GLObject {
 public:
     gl::ShaderProgram shaderProgram;
     GLuint VAO, VBO;
     GLuint texture;
-        
+    mx::Model cube;        
+    mx::Font font;
     Cube() = default;
     virtual ~Cube() override {
-        glDeleteVertexArrays(1, &VAO);
-        glDeleteBuffers(1, &VBO);
-        glDeleteTextures(1, &texture);
     }
 
     std::vector<GLfloat> cubeData;
 
     virtual void load(gl::GLWindow *win) override {     
-        size_t vertexCount = sizeof(vertices) / (3 * sizeof(GLfloat)); 
-        size_t texCoordCount = sizeof(texCoords) / (2 * sizeof(GLfloat)); 
-   
-        if (vertexCount != texCoordCount) {
-            throw std::runtime_error("Vertices and texture coordinates size mismatch");
+        font.loadFont(win->util.getFilePath("data/font.ttf"), 18);
+        if(cube.openModel(win->util.getFilePath("data/cube.mxmod.z")) == false) {
+            throw mx::Exception("Error loading model");
         }
-     
-        cubeData.resize(vertexCount * 5);   
-
-        for (size_t i = 0; i < vertexCount; ++i) {
-            cubeData[i * 5] = vertices[i * 3];         // Position X
-            cubeData[i * 5 + 1] = vertices[i * 3 + 1]; // Position Y
-            cubeData[i * 5 + 2] = vertices[i * 3 + 2]; // Position Z
-            cubeData[i * 5 + 3] = texCoords[i * 2];    // Texture U
-            cubeData[i * 5 + 4] = texCoords[i * 2 + 1]; // Texture V
-        }
-
-        glGenVertexArrays(1, &VAO);
-        glGenBuffers(1, &VBO);
-        glBindVertexArray(VAO);
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, cubeData.size() * sizeof(GLfloat), cubeData.data(), GL_STATIC_DRAW);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(0);
-
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-        glEnableVertexAttribArray(1);
-
-        glBindVertexArray(0);
-
-        if (!shaderProgram.loadProgram(win->util.getFilePath("data/tri.vert"), win->util.getFilePath("data/tri.frag"))) {
+        if (!shaderProgram.loadProgramFromText(g_vSource, g_fSource)) {
             throw mx::Exception("Failed to load shader program");
         }
-
         shaderProgram.useProgram();
-
-        glm::mat4 model = glm::mat4(1.0f); // Identity matrix
-       // glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -5.0f)); // Camera backward
-        glm::mat4 view = glm::lookAt(
-            glm::vec3(0.0f, 0.0f, 5.0f),  // Camera position
-            glm::vec3(0.0f, 0.0f, 0.0f),  // Look at origin
-            glm::vec3(0.0f, 1.0f, 0.0f)   // Up vector
-        );
-        glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)win->w / win->h, 0.1f, 100.0f);
-
-        shaderProgram.setUniform("model", model);
-        shaderProgram.setUniform("view", view);
-        shaderProgram.setUniform("projection", projection);
-
-
-        glGenTextures(1, &texture);
-        glBindTexture(GL_TEXTURE_2D, texture);
-        glBindTexture(GL_TEXTURE_2D, texture);
-
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-        SDL_Surface *surface = png::LoadPNG(win->util.getFilePath("data/bg.png").c_str());
-        if (!surface) {
-            mx::system_err << "Error: loading PNG.\n";
-            exit(EXIT_FAILURE);
-        }
-
-        GLenum format = (surface->format->BytesPerPixel == 4) ? GL_RGBA : GL_RGB;
-        glTexImage2D(GL_TEXTURE_2D, 0, format, surface->w, surface->h, 0, format, GL_UNSIGNED_BYTE, surface->pixels);
-        SDL_FreeSurface(surface);
+        cube.setTextures(win, win->util.getFilePath("data/cube.tex"), win->util.getFilePath("data"));
     }
 
     virtual void draw(gl::GLWindow *win) override {
         shaderProgram.useProgram();
-        CHECK_GL_ERROR();
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture);
-        glBindVertexArray(VAO);
         CHECK_GL_ERROR();
 #ifdef __EMSCRIPTEN__
         static Uint32 lastTime = emscripten_get_now(); 
         float currentTime = emscripten_get_now();
         float deltaTime = (currentTime - lastTime) / 1000.0f;
         lastTime = currentTime;
-
 #else
         static Uint32 lastTime = SDL_GetTicks(); 
         Uint32 currentTime = SDL_GetTicks();
@@ -223,26 +162,80 @@ public:
 #endif
         static float rotationAngle = 0.0f;
         rotationAngle += deltaTime * 50.0f; 
-        glm::mat4 model = glm::rotate(glm::mat4(1.0f), glm::radians(rotationAngle), glm::vec3(0.0f, 1.0f, 0.0f));
-        shaderProgram.setUniform("model", model);
+        cube.setShaderProgram(&shaderProgram, "texture1");
+        shaderProgram.useProgram();
+        cameraPosition = glm::vec3(0.0f, 2.0f, 5.0f);
+        glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+        glm::vec3 upVector = glm::vec3(0.0f, 1.0f, 0.0f);
+        modelMatrix = glm::mat4(1.0f);
+        modelMatrix = glm::scale(modelMatrix, glm::vec3(2.0f, 2.0f, 2.0f));
+        modelMatrix = glm::rotate(modelMatrix, glm::radians(rotationAngle), glm::vec3(rot_x, 1.0f, 0.0f));
+        viewMatrix = glm::lookAt(cameraPosition, cameraTarget, upVector);
+        float aspectRatio = static_cast<float>(win->w) / static_cast<float>(win->h);
+        projectionMatrix = glm::perspective(glm::radians(45.0f), aspectRatio, 0.1f, 100.0f);      
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram.id(), "model"), 1, GL_FALSE, glm::value_ptr(modelMatrix));
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram.id(), "view"), 1, GL_FALSE, glm::value_ptr(viewMatrix));
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram.id(), "projection"), 1, GL_FALSE, glm::value_ptr(projectionMatrix));
+        glUniform3f(glGetUniformLocation(shaderProgram.id(), "lightPos"), lightPos.x, lightPos.y, lightPos.z);
+        glUniform3f(glGetUniformLocation(shaderProgram.id(), "viewPos"), cameraPosition.x, cameraPosition.y, cameraPosition.z);
+        glUniform3f(glGetUniformLocation(shaderProgram.id(), "lightColor"), 1.0f, 1.0f, 1.0f);
+        glUniform3f(glGetUniformLocation(shaderProgram.id(), "objectColor"), 1.0f, 1.0f, 1.0f); 
         CHECK_GL_ERROR();
-        size_t vertexCount = cubeData.size() / 5; 
-        glDrawArrays(GL_TRIANGLES, 0, vertexCount);
-        CHECK_GL_ERROR();
-        glBindVertexArray(0);
-        CHECK_GL_ERROR();
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glDepthMask(GL_FALSE);
+        cube.drawArrays();
+        glDepthMask(GL_TRUE);
+        glDisable(GL_BLEND);
+        win->text.setColor({255, 255, 255, 255});
+        win->text.printText_Solid(font, 25.0f, 25.0f, "Press Arrow keys / Page Up Down to move light source Enter to reset");
+        win->text.printText_Solid(font, 25.0f, 60.0f, "Press Space to rotate cube");
     }
   
-
-    virtual void event(gl::GLWindow *window, SDL_Event &e) override {
+    void event(gl::GLWindow *win, SDL_Event &e) override {
+        if (e.type == SDL_KEYDOWN) {
+            float moveSpeed = 0.05f; 
+            switch (e.key.keysym.sym) {
+                case SDLK_LEFT:
+                    lightPos.x -= moveSpeed;
+                    break;
+                case SDLK_RIGHT:
+                    lightPos.x += moveSpeed;
+                    break;
+                case SDLK_UP:
+                    lightPos.z -= moveSpeed;
+                    break;
+                case SDLK_DOWN:
+                    lightPos.z += moveSpeed;
+                    break;
+                case SDLK_PAGEUP:
+                    lightPos.y += moveSpeed;
+                    break;
+                case SDLK_PAGEDOWN:
+                    lightPos.y -= moveSpeed;
+                    break;
+                case SDLK_RETURN:
+                    lightPos = glm::vec3(0.0f, 5.0f, 0.0f);
+                    break;
+                case SDLK_SPACE:
+                    if(rot_x >  0) rot_x = 0; else rot_x = 1.0f;
+                    break;
+            }
+            std::cout << "Light Position: " << lightPos.x << ", " << lightPos.y << ", " << lightPos.z << "\n";
+        }
     }
 private:
-    
+    glm::mat4 modelMatrix{1.0f};
+    glm::mat4 viewMatrix{1.0f};
+    glm::mat4 projectionMatrix{1.0f};
+    glm::vec3 cameraPosition{0.0f, 2.0f, 5.0f};
+    glm::vec3 lightPos{0.0f, 5.0f, 0.0f};
+    float rot_x =  1.0f;
 };
 
 class MainWindow : public gl::GLWindow {
 public:
-    MainWindow(std::string path, int tw, int th) : gl::GLWindow("OpenGL Example", tw, th) {
+    MainWindow(std::string path, int tw, int th) : gl::GLWindow("Transparent Rotating Cube", tw, th) {
         setPath(path);
         setObject(new Cube());
 		object->load(this);
