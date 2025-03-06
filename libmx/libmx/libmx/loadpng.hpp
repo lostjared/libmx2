969f31055a069fbd1affbd347b5d23346708de96
Jared Bruni <jaredbruni@gmail.com>
2025-03-06 15:12:44 -0800

    Delete libmx/libmx/libmx directory

--- libmx/libmx/libmx/loadpng.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __LOADPNG_H__
-#define __LOADPNG_H__
-#include"SDL.h"
-namespace png {
-    SDL_Surface *LoadPNG(const char* file);
-    bool SavePNG(SDL_Texture* texture, SDL_Renderer* renderer, const char* filename);
-    bool SavePNG_RGBA(const char *filename, void *buffer, int w, int h);
-}
-#endif
