31962c5c5f574806b2170acce6f2d2883590aa39
Jared Bruni <jaredbruni@gmail.com>
2025-04-09 21:07:31 -0700

    updated

--- model_viewer/model_view.cpp
+++ model_viewer/model_view.cpp
@@ -7,17 +7,17 @@ void MainWindow::initWindow()
     setMinimumSize(800, 600);
     setMaximumSize(1600, 1200);
     setWindowFlags(Qt::Window | Qt::WindowTitleHint | Qt::CustomizeWindowHint | Qt::WindowCloseButtonHint | Qt::WindowMinMaxButtonsHint);
-    setAttribute(Qt::WA_DeleteOnClose);
-    setAttribute(Qt::WA_QuitOnClose, true);
-    setAttribute(Qt::WA_NativeWindow, true);
-    setAttribute(Qt::WA_TranslucentBackground, false);
-    setAttribute(Qt::WA_OpaquePaintEvent, true);
-    setAttribute(Qt::WA_NoSystemBackground, false);
-    setAttribute(Qt::WA_PaintOnScreen, true);
-    setAttribute(Qt::WA_InputMethodEnabled, true);
-    setAttribute(Qt::WA_InputMethodTransparent, false);
-    setAttribute(Qt::WA_WState_Visible, true);
-    setAttribute(Qt::WA_WState_Hidden, false);
+//    setAttribute(Qt::WA_DeleteOnClose);
+//    setAttribute(Qt::WA_QuitOnClose, true);
+//    setAttribute(Qt::WA_NativeWindow, true);
+//    setAttribute(Qt::WA_TranslucentBackground, false);
+//    setAttribute(Qt::WA_OpaquePaintEvent, true);
+//    setAttribute(Qt::WA_NoSystemBackground, false);
+//    setAttribute(Qt::WA_PaintOnScreen, true);
+//    setAttribute(Qt::WA_InputMethodEnabled, true);
+//    setAttribute(Qt::WA_InputMethodTransparent, false);
+//    setAttribute(Qt::WA_WState_Visible, true);
+//    setAttribute(Qt::WA_WState_Hidden, false);
     
     
     setupFileSelectionUI();
