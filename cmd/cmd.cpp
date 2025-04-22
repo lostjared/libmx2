9f998dedd0853668aee0489ddbfe8d7d8ac79c89
Jared Bruni <jaredbruni@gmail.com>
2025-04-22 12:15:14 -0700

    use readline
    use readline

--- cmd/cmd.cpp
+++ cmd/cmd.cpp
@@ -1,4 +1,3 @@
-#include"linenoise.h"
 #include"ast.hpp"
 #include"parser.hpp"
 #include"scanner.hpp"
@@ -13,27 +12,36 @@
 #include<functional>
 #include<unordered_map>
 
-int main(int argc, char **argv) {
+// Add readline includes
+#include <readline/readline.h>
+#include <readline/history.h>
 
-    char *line = nullptr;
+int main(int argc, char **argv) {
     bool active = true;
 
     try {
         cmd::AstExecutor executor{};
-        linenoiseHistoryLoad("cmd-history.txt");
+        using_history();
+        read_history(".cmd_history");
         while(active) {
             try {
                 std::string prompt = executor.getPath() + "> ";
-                line = linenoise(prompt.c_str());
-                std::string command_data(line);
-                free(line);
-                linenoiseHistoryAdd(command_data.c_str());
-                linenoiseHistorySave("cmd-history.txt");
-                scan::TString string_buffer(command_data);
-                scan::Scanner scanner(string_buffer);
-                cmd::Parser parser(scanner);
-                auto ast = parser.parse();
-                executor.execute(ast);
+                char* line = readline(prompt.c_str());
+                if (line == nullptr) {
+                    std::cout << std::endl;  
+                    break;
+                }
+                if (line[0] != '\0') {
+                    add_history(line);
+                    std::string command_data(line);
+                    free(line);
+                    scan::TString string_buffer(command_data);
+                    scan::Scanner scanner(string_buffer);
+                    cmd::Parser parser(scanner);
+                    auto ast = parser.parse();
+                    executor.execute(ast);
+                }
+                
             } catch (const scan::ScanExcept &e) {
                 std::cerr << "Scan error: " << e.why() << std::endl;
             } catch (const std::exception &e) {
@@ -42,6 +50,7 @@ int main(int argc, char **argv) {
                 std::cerr << "Unknown error occurred." << std::endl;
             }
         }
+        write_history(".cmd_history");
     } catch (const scan::ScanExcept &e) {
         std::cerr << "Scan error: " << e.why() << std::endl;
     } catch (const std::exception &e) {
