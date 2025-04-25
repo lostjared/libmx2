6a63063e4c91affbd9211be5e2d5b55d1d49477f
Jared Bruni <jaredbruni@gmail.com>
2025-04-25 14:21:02 -0700

    command
    state exception

--- cmd/src/cmd.cpp
+++ cmd/src/cmd.cpp
@@ -63,6 +63,9 @@ int main(int argc, char **argv) {
                 std::cerr << "Scan error: " << e.why() << std::endl;
             } catch (const std::exception &e) {
                 std::cerr << "Error: " << e.what() << std::endl;
+            }  catch (const state::StateException &e) {
+                std::cerr << "State error: " << e.what() << std::endl;
+                return 1;
             } catch (...) {
                 std::cerr << "Unknown error occurred." << std::endl;
             }
