c021eceddb1d6272f7caf645836b912bce75a64e
Jared Bruni <jaredbruni@gmail.com>
2025-05-08 12:42:36 -0700

    Updated page

--- cmd/src/page.cpp
+++ cmd/src/page.cpp
@@ -17,9 +17,27 @@ public:
             scan::Scanner scanner(string_buffer);
             cmd::Parser parser(scanner);
             auto ast = parser.parse();
-            std::cout << "Debug Information written to: debug.html\n\n";
             std::ostringstream stream;
             html::gen_html(stream, ast);
+            return stream.str();
+        } catch(const scan::ScanExcept &e) {
+            return "Scan Error: " + e.why() + "\n";    
+        } catch(const std::exception &e) {
+            return "Exception: " + std::string(e.what()) + "\n";
+        } catch(...) {
+            return  "Unknown Error has Occoured..\n";
+        }
+        return "[Unknown Error]";
+    }
+
+    std::string outputFormatHTML(const std::string &input) {
+        try {
+            scan::TString string_buffer(input);
+            scan::Scanner scanner(string_buffer);
+            cmd::Parser parser(scanner);
+            auto ast = parser.parse();
+            std::ostringstream stream;
+            html::gen_html_color(stream, ast);
             return stream.str();
         } catch(const scan::ScanExcept &e) {
             return "Scan Error: " + e.why() + "\n";    
@@ -35,5 +53,6 @@ public:
 EMSCRIPTEN_BINDINGS(export_html) {
     emscripten::class_<ExportHTML>("ExportHTML")
         .constructor<>()
-        .function("outputHTML", &ExportHTML::outputHTML);
+        .function("outputHTML", &ExportHTML::outputHTML)
+        .function("outputFormatHTML", &ExportHTML::outputFormatHTML);
 }
