8a4b5c303ae77fb25fc5d16e2b67d4306d0fdfb0
Jared Bruni <jaredbruni@gmail.com>
2025-10-07 16:59:37 -0700

    green crosshairs

--- libmx/gl_walktest/room.cpp
+++ libmx/gl_walktest/room.cpp
@@ -1949,7 +1949,7 @@ public:
             out vec4 FragColor;
             
             void main() {
-                FragColor = vec4(1.0, 1.0, 1.0, 0.8);
+                FragColor = vec4(0.0, 1.0, 0.0, 0.8);
             }
         )";
 #else
