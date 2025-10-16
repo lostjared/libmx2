a3a73ef7e763247a360cf3f106de9b7590b38fe4
Jared Bruni <jaredbruni@gmail.com>
2025-10-16 03:35:57 -0700

    updated code

--- libmx/gl_walktest/room.cpp
+++ libmx/gl_walktest/room.cpp
@@ -1065,6 +1065,7 @@ public:
         )";
         
         
+        
         const char *fragmentShader = R"(#version 330 core
 
             in vec2 TexCoord;
@@ -1095,7 +1096,60 @@ public:
 
                 color = vec4(finalColor, texColor.a);
             }
-  )";
+  )"; 
+/*
+  const char *fragmentShader = R"(#version 330 core
+
+in vec2 TexCoord;
+out vec4 color;
+uniform sampler2D floorTexture;
+uniform float time_f;
+uniform float alpha;
+
+void main(void) {
+    vec2 tc = TexCoord;
+    float rippleSpeed = 5.0;
+    float rippleAmplitude = 0.03;
+    float rippleWavelength = 10.0;
+    float twistStrength = 1.0;
+    float radius = length(tc - vec2(0.5, 0.5));
+    float ripple = sin(tc.x * rippleWavelength + time_f * rippleSpeed) * rippleAmplitude;
+    ripple += sin(tc.y * rippleWavelength + time_f * rippleSpeed) * rippleAmplitude;
+    vec2 rippleTC = tc + vec2(ripple, ripple);
+    
+    float angle = twistStrength * (radius - 1.0) + time_f;
+    float cosA = cos(angle);
+    float sinA = sin(angle);
+    mat2 rotationMatrix = mat2(cosA, -sinA, sinA, cosA);
+    vec2 twistedTC = (rotationMatrix * (tc - vec2(0.5, 0.5))) + vec2(0.5, 0.5);
+    
+    vec4 originalColor = texture(floorTexture, tc);
+    vec4 twistedRippleColor = texture(floorTexture, mix(rippleTC, twistedTC, 0.5));
+    color = mix(originalColor, twistedRippleColor, 0.5);
+    color = twistedRippleColor;
+      })";*/
+
+    
+      /*
+
+  const char *fragmentShader = R"(#version 330 core
+
+in vec2 TexCoord;
+out vec4 color;
+uniform sampler2D floorTexture;
+uniform float time_f;
+
+void main(void) {
+    vec2 tc = TexCoord;
+    vec2 center = vec2(0.5, 0.5);
+    vec2 uv = tc - center;
+    float dist = length(uv);
+    float ripple = sin(10.0 * dist - time_f * 6.28318) * 0.1;
+    uv += uv * sin(ripple * time_f);
+    uv += center * atan(dist * time_f);
+    color = texture(floorTexture, uv);
+})";
+*/
   
 /*
   const char *fragmentShader = R"(#version 330 core
