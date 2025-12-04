3b8300cf524314f5d0c78bf0aa17f88ddc1b1d58
Jared Bruni <jaredbruni@gmail.com>
2025-12-04 08:25:10 -0800

    updated code for gl_about

--- libmx/gl_about/about.cpp
+++ libmx/gl_about/about.cpp
@@ -1038,7 +1038,31 @@ void main(void) {
     color = texture(textTexture, warpedCoord);
 })";
 
-
+const char *szGeo = R"(#version 300 es
+precision highp float;
+out vec4 color;
+in vec2 TexCoord;
+uniform sampler2D textTexture;
+uniform vec2 iResolution;
+uniform float time_f;
+void main(void) {
+    vec2 tc = TexCoord;
+    vec2 uv = tc * iResolution / vec2(iResolution.y);
+    float time = time_f * 0.5;
+    float time_t = mod(time_f, 15.0);
+    float angle = time;
+    vec2 center = vec2(0.5, 0.5) * iResolution / vec2(iResolution.y);
+    vec2 toCenter = uv - center;
+    float radius = length(toCenter);
+    float theta = atan(toCenter.y, toCenter.x) + time;
+    float pattern = abs(sin(12.0 * theta) * cos(12.0 * radius));
+    vec3 colorShift = vec3(0.5 * sin(time) + 0.5, 0.5 * cos(time) + 0.5, sin(radius - time));
+    vec3 finalColor = (0.2 + 0.8 * pattern) + colorShift * pattern;
+    vec4 text_color = texture(textTexture, tc);
+    vec4 fc = vec4(finalColor, 1.0);
+    color = sin(mix(text_color, fc, 0.3) * length(time_t));
+    color.a = 1.0;
+})";
 
 class About : public gl::GLObject {
     GLuint texture = 0;
@@ -1056,11 +1080,11 @@ public:
     }
 
     void load(gl::GLWindow *win) override {
-        texture = gl::loadTexture(win->util.getFilePath("data/jaredkale.png"));
+        texture = gl::loadTexture(win->util.getFilePath("data/alien1.png"));
         if(texture == 0) {
             throw mx::Exception("Error loading texture");
         }
-        if(!shader.loadProgramFromText(gl::vSource, szDeform)) {
+        if(!shader.loadProgramFromText(gl::vSource, szGeo)) {
             throw mx::Exception("Error loading texture");
         }
         shader.useProgram();
