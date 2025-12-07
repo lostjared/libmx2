ad9096be72548821f682f3e9d3993a67a64ecfbf
Jared Bruni <jaredbruni@gmail.com>
2025-12-07 08:59:43 -0800

    possible leak fixed

--- libmx/libmx/gl.cpp
+++ libmx/libmx/gl.cpp
@@ -826,9 +826,6 @@ namespace gl {
             throw mx::Exception("Invalid screen dimensions in GLSprite::initWithTexture");
         }
         this->shader = program;
-        if(this->texture != 0) {
-            glDeleteTextures(1, &this->texture);
-        }
         this->texture = text;
         float ndcX = (x / screenWidth) * 2.0f - 1.0f;       
         float ndcY = 1.0f - (y / screenHeight) * 2.0f;      
@@ -840,15 +837,20 @@ namespace gl {
             ndcX,       ndcY - h,   0.0f, 0.0f, 0.0f,  
             ndcX + w,   ndcY - h,   0.0f, 1.0f, 0.0f   
         };
-        glGenVertexArrays(1, &VAO);
-        glGenBuffers(1, &VBO);
-        glBindVertexArray(VAO);
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-        glEnableVertexAttribArray(1);
+        if (VAO == 0) {
+            glGenVertexArrays(1, &VAO);
+            glGenBuffers(1, &VBO);
+            glBindVertexArray(VAO);
+            glBindBuffer(GL_ARRAY_BUFFER, VBO);
+            glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_DYNAMIC_DRAW);
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+            glEnableVertexAttribArray(0);
+            glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+            glEnableVertexAttribArray(1);
+        } else {
+            glBindBuffer(GL_ARRAY_BUFFER, VBO);
+            glBufferSubData(GL_ARRAY_BUFFER, 0, vertices.size() * sizeof(float), vertices.data());
+        }
         this->shader->useProgram();
         this->shader->setUniform(textureName, 0);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
