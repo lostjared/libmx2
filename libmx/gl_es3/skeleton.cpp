b263fe8533894e137b7a51cd8407ac4a0d4a3ffa
Jared Bruni <jaredbruni@gmail.com>
2026-01-09 00:30:22 -0800

    :using GLES 3 on desktop use GLMode enum in constructor for GLWindow

---
+++ libmx/gl_es3/skeleton.cpp
@@ -0,0 +1,100 @@
+#include"mx.hpp"
+#include"argz.hpp"
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten/emscripten.h>
+#include <GLES3/gl3.h>
+#endif
+
+#include"gl.hpp"
+#include"loadpng.hpp"
+
+#define CHECK_GL_ERROR() \
+{ GLenum err = glGetError(); \
+if (err != GL_NO_ERROR) \
+printf("OpenGL Error: %d at %s:%d\n", err, __FILE__, __LINE__); }
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+
+class Game : public gl::GLObject {
+public:
+    Game() = default;
+    virtual ~Game() override {}
+
+    void load(gl::GLWindow *win) override {
+        font.loadFont(win->util.getFilePath("data/font.ttf"), 36);
+    }
+
+    void draw(gl::GLWindow *win) override {
+        glDisable(GL_DEPTH_TEST);
+        Uint32 currentTime = SDL_GetTicks();
+        float deltaTime = (currentTime - lastUpdateTime) / 1000.0f; 
+        lastUpdateTime = currentTime;
+        win->text.setColor({255, 255, 255, 255});
+        win->text.printText_Solid(font, 25.0f, 25.0f, "Hello, World! from OpenGL: " + std::to_string(currentTime));
+        update(deltaTime);
+    }
+    
+    void event(gl::GLWindow *win, SDL_Event &e) override {}
+    void update(float deltaTime) {}
+private:
+    mx::Font font;
+    Uint32 lastUpdateTime = SDL_GetTicks();
+};
+
+class MainWindow : public gl::GLWindow {
+public:
+    MainWindow(std::string path, int tw, int th) : gl::GLWindow("Skeleton", tw, th, true, gl::GLMode::ES) {
+        setPath(path);
+        setObject(new Game());
+        object->load(this);
+    }
+    
+    ~MainWindow() override {}
+    
+    virtual void event(SDL_Event &e) override {}
+    
+    virtual void draw() override {
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glEnable(GL_DEPTH_TEST);
+        glViewport(0, 0, w, h);
+        object->draw(this);
+        swap();
+        delay();
+    }
+};
+
+MainWindow *main_w = nullptr;
+
+void eventProc() {
+    main_w->proc();
+}
+
+int main(int argc, char **argv) {
+#ifdef __EMSCRIPTEN__
+    try {
+        MainWindow main_window("/", 960, 720);
+        main_w =&main_window;
+        emscripten_set_main_loop(eventProc, 0, 1);
+    } catch(const mx::Exception &e) {
+        mx::system_err << "mx: Exception: " << e.text()  << "\n";
+        mx::system_err.flush();
+        exit(EXIT_FAILURE);
+    }
+#else
+    Arguments args = proc_args(argc, argv);
+    try {
+        MainWindow main_window(args.path, args.width, args.height);
+        main_window.loop();
+    } catch(const mx::Exception &e) {
+        mx::system_err << "mx: Exception: " << e.text() << "\n";
+        mx::system_err.flush();
+        exit(EXIT_FAILURE);
+    }
+#endif
+    return 0;
+}
