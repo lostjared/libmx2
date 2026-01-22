b541a78d88cf62328fbd487c9dd21cbcc1cfa7e2
Jared Bruni <jaredbruni@gmail.com>
2026-01-22 00:58:42 -0800

    4.1

--- libmx/libmx/gl.cpp
+++ libmx/libmx/gl.cpp
@@ -130,8 +130,8 @@ namespace gl {
 #ifndef __EMSCRIPTEN__
 
         if(gl_mode == GLMode::DESKTOP) {
-            SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+            SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
 	        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
             SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
             SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
