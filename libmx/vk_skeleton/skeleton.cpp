817a5aa0e474f400df0455f69f3fb4213b510837
Jared Bruni <jaredbruni@gmail.com>
2026-01-28 20:18:04 -0800

    skeleton

---
+++ libmx/vk_skeleton/skeleton.cpp
@@ -0,0 +1,58 @@
+#include "vk.hpp"
+#include "SDL.h"
+
+#if defined(__APPLE__) || defined(_WIN32) || defined(__linux__)
+#include "argz.hpp"
+#endif
+
+class SkeletonWindow : public mx::VKWindow {
+public:
+    SkeletonWindow(const std::string& path, int wx, int wy, bool full) 
+        : mx::VKWindow("-[ Vulkan Skeleton ]-", wx, wy, full) {
+        setPath(path);
+    }
+    
+    virtual ~SkeletonWindow() {}
+    
+    void initVulkan() override {
+        mx::VKWindow::initVulkan();
+    }
+    
+    void proc() override {
+        printText("Hello World", 50, 50, {255, 255, 255, 255});
+    }
+    
+    void event(SDL_Event& e) override {
+        if (e.type == SDL_QUIT || (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE)) {
+            quit();
+        }
+    }
+
+private:
+};
+
+int main(int argc, char **argv) {
+#if defined(__APPLE__) || defined(_WIN32) || defined(_WIN64) || defined(__linux__)
+#ifndef __ANDROID__
+    Arguments args = proc_args(argc, argv);
+    try {
+        SkeletonWindow window(args.path, args.width, args.height, args.fullscreen);
+        window.initVulkan();
+        window.loop();
+        window.cleanup();
+    } catch (mx::Exception &e) {
+        SDL_Log("mx: Exception: %s\n", e.text().c_str());
+    }
+#endif
+#elif defined(__ANDROID__)
+    try {
+        SkeletonWindow window("", 960, 720, false);
+        window.initVulkan();
+        window.loop();
+        window.cleanup();
+    } catch (mx::Exception &e) {
+        SDL_Log("mx: Exception: %s\n", e.text().c_str());
+    }
+#endif
+    return EXIT_SUCCESS;
+}
