3be37e0322153db71e6c8679a4065e205156fb8e
Jared Bruni <jaredbruni@gmail.com>
2026-02-02 00:28:01 -0800

    compatibility with macos

--- libmx/vk_tex_cube/vk.cpp
+++ libmx/vk_tex_cube/vk.cpp
@@ -230,7 +230,7 @@ namespace mx {
 
 #ifdef WITH_MOLTEN
         
-        extensions.push_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+        //extensions.push_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
         extensions.push_back(VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME);
 #endif
 
