474a1787265e9a60a2e0406c2a494a85b21953e6
Jared Bruni <jaredbruni@gmail.com>
2026-02-06 08:49:53 -0800

    food

---
+++ libmx/libmx/vk_spacerox/CMakeFiles/vk_spacerox.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vk_spacerox.dir/link.d"
+  "CMakeFiles/vk_spacerox.dir/space.cpp.o"
+  "CMakeFiles/vk_spacerox.dir/space.cpp.o.d"
+  "CMakeFiles/vk_spacerox.dir/vk.cpp.o"
+  "CMakeFiles/vk_spacerox.dir/vk.cpp.o.d"
+  "vk_spacerox"
+  "vk_spacerox.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vk_spacerox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
